7db80a234eec45609d641b63d0ce4a774bd4631f
Richard Chan <richardchanme@gmail.com>
2017-07-31 23:54:58 -0700

    create onclick for add button, added class for each new list item, and input form clears off after adding item

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 function addItem() {
 	const input = document.getElementById('input').value;
+	// if user input is empty, don't add anything to to-do list
+	if (input != '') {
 	const body = document.getElementById('toDoList');
 	const div = document.createElement('div');
-	const text = document.createElement('h3');
+	const text = document.createElement('li');
+	text.className = 'list-group-item col-md-4 col-md-offset-4';
 
 	text.innerHTML = input;
 	body.appendChild(div);
 	div.appendChild(text);
+	}
+	// clears out input form after content has been added
+	document.getElementById('input').value = '';
+}
+// adds functionality to Add button
+document.getElementById('add').onclick = function() {
+	addItem(); // calls addItem function
+};
+
+// create delete button next to new to-do item
+function deleteItem(item) {
+	const remove = document.getElementById(item);
+
 }
 
+// adds input on Enter/Return key
 const addList = document.getElementById('input');
-
 addList.addEventListener('keypress', function(e) {
 	var key = e.which || e.keyCode;
 	if (key === 13) {
